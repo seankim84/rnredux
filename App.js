aebaee6ec17148a82fb7542f0e7533050069e14d
SeanKim <sean.kim84@gmail.com>
2018-08-07 12:57:54 +0700

    mapDispatchToProps for getPeople

--- App.js
+++ App.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableHighlight, Text, View, StyleSheet } from 'react-native';
+import {connect} from 'react-redux';
+import fetchPeopleFromAPI from  "./actions";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+let styles
+
+const App = (props) => {
+  const {
+    container,
+    text,
+    button,
+    buttonText
+  } = styles
+  
+  return (
+    <View style={container}>
+      <Text style={text}>Redux App</Text>
+      <TouchableHighlight style={button}>
+        <Text style={buttonText}>Fetch Data</Text>
+      </TouchableHighlight>
+    </View>
+  )
+}
+
+styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+    paddingLeft: 20,
+    paddingTop: 20
+  },
+
+  text: {
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: "#0b7eff",
+    height: 60,
+    justifyContent: 'center',
+    alignItems: "center"
+  },
+  buttonText: {
+    color: "white"
+  }
+})
+
+function mapStateToProps(state) {
+  return {
+    people: state.people
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function mapDispatchToProps(dispatch) {
+  return {
+    getPeople: () => dispatch(fetchPeopleFromAPI)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
